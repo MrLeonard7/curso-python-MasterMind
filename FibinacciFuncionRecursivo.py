3e791c6127ae4335f6b1901d75d34370d79e1a82
DevLeo <dbecerra655@gmail.com>
2024-01-19 21:50:03 -0500

    Se agrega Funcin de Potencia

--- FibinacciFuncionRecursivo.py
+++ FibinacciFuncionRecursivo.py
@@ -4,9 +4,20 @@ def fibonacci_recursivo(n):
     return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
 
 
+def potencia(numero, base=2):
+    resultado = numero
+    for a in range(1, base):
+        resultado *= numero
+    return resultado
+
+
 def main():
+    print(potencia(4))
+    print(potencia(4, 5))
+    """
     for a in range(10):
         print(fibonacci_recursivo(a))
+"""
 
 
 if __name__ == "__main__":
