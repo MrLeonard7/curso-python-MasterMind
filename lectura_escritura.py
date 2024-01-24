218232bbbdb3c47d09e77730114b8e7d57599f84
DevLeo <dbecerra655@gmail.com>
2024-01-23 22:46:12 -0500

    Add gitignore files .txt

--- lectura_escritura.py
+++ lectura_escritura.py
@@ -1,6 +1,4 @@
-LISTA_DE_PRODUCTOS_DISPONIBLES = ["Pan", "Pollo", "Pipas"]
 SALIDA = "SALIR"
-COMANDO_LISTA = "LISTA"
 
 
 def guardado_de_archivo(lista_compra_usuario):
@@ -12,30 +10,27 @@ def guardado_de_archivo(lista_compra_usuario):
 
 def preguntar_producto_usuario():
     return input("\nIntroduce un producto [{} para salir]\n"
-                 "[{} para lista de productos disponibles]\n"
-                 "->".format(SALIDA, COMANDO_LISTA))
+                 "->".format(SALIDA))
+
+def guardar_item_en_lista(input_usuario):
+    lista_compra = []
+    while input_usuario.upper() != SALIDA:
+        if input_usuario.capitalize() not in [a.capitalize() for a in lista_compra]:
+            lista_compra.append(input_usuario.capitalize())
+            print("\nSe ha añadido {}".format(input_usuario.capitalize()))
+            print("\n".join(lista_compra))
+            input_usuario = preguntar_producto_usuario()
+        else:
+            print("\n{} ya esta en la lista.".format(input_usuario.capitalize()))
+            input_usuario = preguntar_producto_usuario()
+
+    return lista_compra
 
 
 def main():
-    lista_compra = []
-
     input_usuario = preguntar_producto_usuario()
 
-    while input_usuario.upper() != SALIDA:
-        if input_usuario.upper() == COMANDO_LISTA:
-            print("Los productos disponibles son:")
-            print("\n".join(LISTA_DE_PRODUCTOS_DISPONIBLES))
-            input_usuario = preguntar_producto_usuario()
-
-        elif input_usuario.title() in LISTA_DE_PRODUCTOS_DISPONIBLES:
-            lista_compra.append(input_usuario.title())
-            print("\nSe ha añadido {}".format(input_usuario.title()))
-            print("\n".join(lista_compra))
-            input_usuario = preguntar_producto_usuario()
-
-        else:
-            print("\n{} no esta disponible en este mommento".format(input_usuario.title()))
-            input_usuario = preguntar_producto_usuario()
+    lista_compra = guardar_item_en_lista(input_usuario)
 
     guardado_de_archivo(lista_compra)
 
